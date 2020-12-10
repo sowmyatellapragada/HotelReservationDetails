f8e79def14a5309b864fcf6a3714c45844a13ce0
sowmyatellapragada <sowmyatellapragada01@gmail.com>
2020-12-10 17:32:21 +0530

    Add project files.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+using System;
+using Hotel.Entities;
+
+
+namespace Hotel
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+
+            Console.Write("Room number: ");
+            int number = int.Parse(Console.ReadLine());
+            Console.Write("Check-in date (dd/MM/yyyy): ");
+            DateTime checkIn = DateTime.Parse(Console.ReadLine());
+            Console.Write("Check-out date (dd/MM/yyyy): ");
+            DateTime checkOut = DateTime.Parse(Console.ReadLine());
+
+
+            Reservation reservation = new Reservation(number, checkIn, checkOut);
+            Console.WriteLine("Reservation: " + reservation);
+
+            Console.WriteLine();
+
+        }
+    }
+}
